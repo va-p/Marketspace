c2cae97f284cb30ab084a1f5b603a8b8422172d4
Vitor <paiva.v@icloud.com>
2023-04-26 20:26:17 -0300

    style :nail_care:(theme): updates theme

--- src/global/themes/theme.ts
+++ src/global/themes/theme.ts
@@ -1,22 +1,15 @@
 export const THEME = {
   COLORS: {
-    RED_DARK: '#BF3B44',
-    RED_MID: '#F3BABD',
-    RED_LIGHT: '#F4E6E7',
+    BLUE: '#364D9D',
+    BLUE_LIGHT: '#647AC7',
 
-    GREEN_DARK: '#639339',
-    GREEN_MID: '#CBE4B4',
-    GREEN_LIGHT: '#E5F0DB',
-
-    GRAY_100: '#1B1D1E',
-    GRAY_200: '#333638',
-    GRAY_300: '#5C6265',
-    GRAY_400: '#B9BBBC',
-    GRAY_500: '#DDDEDF',
-    GRAY_600: '#EFF0F0',
-    GRAY_700: '#FAFAFA',
-
-    WHITE: '#FFFFFF',
+    GRAY_1: '#1A181B',
+    GRAY_200: '#3E3A40',
+    GRAY_300: '#5F5B62',
+    GRAY_400: '#9F9BA1',
+    GRAY_500: '#D9D8DA',
+    GRAY_600: '#EDECEE',
+    GRAY_700: '#F7F7F8',
   },
   FONTS: {
     REGULAR: 'NunitoSans_400Regular',
